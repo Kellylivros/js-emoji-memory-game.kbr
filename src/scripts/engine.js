c9dc399b25e0ae635eb925906c76fa7141380fd3
Kellylivros <kellylivros@gmail.com>
2024-11-29 15:52:41 -0300

    Commit inicial

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -18,24 +18,18 @@ const emojis = [
 ];
 
 let openCards = [];
-
-// Embaralha as imagens
 let shuffledEmojis = emojis.sort(() => Math.random() - 0.5);
-
-// Adiciona as cartas no tabuleiro
 for (let i = 0; i < shuffledEmojis.length; i++) {
     let box = document.createElement("div");
     box.className = "item";
     box.dataset.emoji = shuffledEmojis[i];
 
-    // Adiciona uma imagem escondida
     let img = document.createElement("img");
     img.src = `./src/images/${shuffledEmojis[i]}`;
-    img.alt = shuffledEmojis[i].split(".")[0]; // Ex: cachorro, gato
-    img.className = "hidden"; // Inicialmente escondido
+    img.alt = shuffledEmojis[i].split(".")[0]; 
+    img.className = "hidden"; 
     box.appendChild(img);
 
-    // Adiciona o evento de clique
     box.onclick = handleClick;
     document.querySelector(".game").appendChild(box);
 }
@@ -44,7 +38,6 @@ function handleClick() {
     if (openCards.length < 2 && !this.classList.contains("boxOpen")) {
         this.classList.add("boxOpen");
 
-        // Mostra a imagem
         const img = this.querySelector("img");
         img.classList.remove("hidden");
 
@@ -64,13 +57,12 @@ function checkMatch() {
         openCards.forEach(card => {
             card.classList.remove("boxOpen");
             const img = card.querySelector("img");
-            img.classList.add("hidden"); // Esconde novamente
+            img.classList.add("hidden"); 
         });
     }
 
     openCards = [];
 
-    // Verifica se todas as cartas foram encontradas
     if (document.querySelectorAll(".boxMatch").length === emojis.length) {
         setTimeout(() => alert("Você venceu!"), 300);
     }
